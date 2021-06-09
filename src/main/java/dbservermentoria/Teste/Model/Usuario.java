e63267404b72eb0139aa27aee9d29a6510233fa2
Ricardo-2021 <ricardo.guimaraes@dbserver.com.br>
2021-06-09 09:41:15 -0300

    atualizacao autenticacao usuario

---
+++ src/main/java/dbservermentoria/Teste/Model/Usuario.java
@@ -0,0 +1,60 @@
+package dbservermentoria.Teste.Model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "\"user\"")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_permission", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @ JoinColumn(name = " permission_id"))
+    private List<Permissao> permissoes;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Permissao> getPermissoes() {
+        return permissoes;
+    }
+
+    public void setPermissoes(List<Permissao> permissoes) {
+        this.permissoes = permissoes;
+    }
+}
